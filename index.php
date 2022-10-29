5791a5493ffdd9afbcc5aa880a892e31bd5063dc
MariaValvede2022 <mariajosevalverdebautista@gmail.comgit config --global user.email mariajosevalverdebautista@gmail.com>
2022-10-29 12:05:08 -0500

    cambio de informacion

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ include("marco-right.php");
   </div>
   <div style="width: 100%; height:100%;" >
            <div  style="text-align:justify; font-size: 20px;">
-                   <p class="eys-parrafo">Hola, yo soy Maria Jose Valverde Bautista, ingeniero esmeraldeño que labora desde hace 10 años en la Universidad Tecnica Luis Vargas Torres, e impartido diferentes cátedras en la carrera de Tecnología de la Información entre las cuales esta Ingeniería de Software, Programación Web, Proyectos informáticos entre otras; pero la cátedra en la que mejor me desempeño es la de Fundamentos de programación que se dicta en el primer ciclo de la carrera de Tecnología de la Información. </p>
+                   <p class="eys-parrafo">Hola, yo soy Maria Jose Valverde Bautista, naci en Esmeraldas-Ecuador,el 16 de Octubre de 1995 mis padres son Esneda Bautista,Jose Valverde mis hermanos son:Jose Elias Sosa,Shirley Sosa,FernandaSosa,Ana Valverde,Diana Valverde.Desde mi niñez resido en la provincia de Esmeraldas estudie la primaria,la secundaria termine el bachillerato con apoyo de mi madre encaminando mis estudios seguir con la Universidad curse el 2do nivel me retire mis estudios por falta de apoyo,trabaje 4años vuelvo a retomar mis estudios por si misma hoy soy estudiante de 7mo nivel de Tecnologia me esfuerzo por aprender lo que me enseñan los profesores ser una persona coherente logrando lo que deseo,culminar mi carrera graduarme y seguir profesionalizandome. </p>
                    <p class="eys-parrafo">Desde mís estudios secundarios me he apacionado por las ciencias y la tecnología, quieza sería  por esta capacidad que tiene estas dos áres del conocimiento de solucionar muchos probemas de la sociedad; es por eso que desde que termine mis estudios de ingeniería en la ESPOL, me he dedicado a la programación de computadoras. </p>
 <p class="eys-parrafo"> El primer lenguaje con el cual empece a programar en el tercer ciclo de universidad fue el <b><a href="https://en.wikipedia.org/wiki/BASIC"> basic</a></b>  para este tiempo la ESPOL contaba con laboratorios de computadora  una computadora personal de marca IBM de tip XT modelo 5160  en es.</p>
 
@@ -34,9 +34,9 @@ include("marco-right.php");
     
 
 
-                   <p class="eys-parrafo">Mis estudios secundarios los realice en el colegio Fisco-Misional Sagado Corazon donde me gradue en el año 1989, de la especialidad de Físico-Matemático, apenas me gradue viaje a Guayaquil para seguir mis estudios universitarios en la Escuela Superior Politécnica del Litoral donde seguí la carrera de Ingeniería en Computación, gracias a la demanda que exitía en este tiempo de este tipo de profesionales comencé a trabajar mientra estaba estudiando, prestando mis servicios profesionales en empresas como Poligráfica, El telegráfo, Intecorsorcio; mientras trabajaba en esta última logré obtener mi título de Ingeniero de Computación.</p>
-                   <p class="eys-parrafo">Ya con el título en la mano decidi regrasar a mi tierra Esmeraldas, con la finalidad de aplicar los conocimiento y habilidades obtenida por lo que forme la empresa EDUCAYSOFT, pero como los ingreso no fueron suficiente entre a trabajar al colegio Eloy Alfaro y posteiormente a la Universidad Técnica Luis Vargas Torres donde actualmente laboró.</p>
-<p class="eys-parrafo"> Por las exigencias de la universidad, ingresar a la maestria de docencia y desarrollo del curriculo, de la cual de grauduo obteniendo el título de Magister, pero despues realice otra maestría en la POLITECNICA, de Ciencias de la Computación.</p>
+                   <p class="eys-parrafo">Mis estudios primarios los realice en la escuela fiscal Luis Prado Viteri,los secundarios los realice en el colegio Instituto Superior 5 de Agosto me gradue en la especialidad Tecnico en comercio y Administracion,superior en la Universidad Tecnica Luis Vargas Torres en la facultad de TICS Tecnologia de la Informacion y Comunicacion.</p>
+                   <p class="eys-parrafo">Ya con el título en la mano aspiro tener mi trabajo en el area de Tecnologiade la Informacion y comunciacion desempeñarme con los conocimientos obtenidos.</p>
+<p class="eys-parrafo"> Por las exigencias de la universidad,en la docencia seguire una maestria en Ciencias de la Computacion impartir mis conocimentos con los estudiantes.</p>
    <div id="blog-seccion3"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mi perfil informático</h2> 
@@ -50,7 +50,7 @@ include("marco-right.php");
     <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
     
 
-                   <p   class="eys-parrafo">Soy amante a la lectura, así que en los ratos libres me encanta actualizar mis conocimientos leyendo manuales de los leguajes de progración que namejo, y tambien muchos noticias de las ultimas tendencias en tecnología.</p>
+                   <p   class="eys-parrafo">Soy amante a la lectura, leyendo libros de programacion, obteniendo conocimentos nuevas ideas,tambien investigar ver las  ultimas noticias de las tendencias tecnologicas.</p>
        
 
                    
