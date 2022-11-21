2cb4446f5b3c5bb18f33ec569bd5b9690552e9c7
MariaValvede2022 <mariajosevalverdebautista@gmail.comgit config --global user.email mariajosevalverdebautista@gmail.com>
2022-11-20 22:46:39 -0500

    Actualizando Perfil

--- head.php
+++ head.php
@@ -11,7 +11,7 @@
  <title> Maria Jose  </title>
 <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-<link rel="icon" href="images/favicon.ico" type="images/x-icon"/> 
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
 <link rel="stylesheet" type="text/css" href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/assets/css/eys.css" />   
 <link rel="stylesheet" type="text/css" href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/assets/src/prettify.css" />   
 <style>
